3a7abc401e6e4fe64bcac18ffb2592ee2d0e7952
Ian Anjos <78967763+IanAguiar-ai@users.noreply.github.com>
2024-01-01 22:41:04 -0300

    First version (functional)

---
+++ server_master.py
@@ -0,0 +1,238 @@
+"""
+Server aberto para requisições e respostas.
+"""
+import socket
+from threading import Thread
+from time import sleep, time
+
+class Server:
+    def __init__(self, host:str = "0.0.0.0", port:int = 12345, limit:int = 3, logic = None):
+        self.points = {}
+        self.equivalent = {"ip":{}, "int":{}}
+        self.print__ = True
+        self.send_to = {}
+        self.condition = {}
+
+        self.logic = logic #Função
+        #text = self.logic(self, text:str, ip:str, port:int) -> str
+        #Recebe:
+        # - objeto server
+        # - texto;
+        # - ip;
+        # - porta.
+        #Retorna:
+        # - texto.
+
+        #Configuração:
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((host, port))
+        self.socket.listen(limit)
+        
+
+    def __repr__(self):
+        print(f"Servidor escutando em {host}:{porta}")
+        return None
+
+    def run_server(self, memory):
+        """
+        A hierarquia da resposta é dada por:
+        1) Está bloqueado <server.points[ip]["block"]>
+        2) Tem texto pedido root <server.send_to[ip]>
+        3) Satisfaz uma condição passada pelo root em self.condition[text]
+        4) Confere na lógica passada ao criar o servidor. <server.logic>
+
+        * Onde <ip> é o ip do ponto que fez a requisição;
+        * <text> é o texto mandado pelo ponto;
+        * <server> é o próprio objeto <self>.
+        """
+        con, client = self.socket.accept() #Abre o socket
+        ip, port = client[0], client[1] #Assim que um ponto conecta separa o ip e a porta
+
+        self.print_(f"Conexão estabelecida com {client}")
+
+        if not ip in self.equivalent["ip"]: #Confere se aquele ip já está registrado na base
+            self.print_("Nova conexão!")
+            len_ = len(self.equivalent["ip"].values())
+            self.equivalent["ip"][ip] = len_
+            self.equivalent["int"][len_] = ip
+            self.send_to[ip] = None
+            self.points[ip] = {"block":False,
+                                   "first_time": time(),
+                                   "last_time": time()}
+        else: #Se já está registrado atualiza o horário de request
+            self.points[ip]["last_time"] = time()
+            
+
+        if not self.points[ip]["block"]: #Se ele não foi bloqueado em algum momento
+            data = con.recv(1024).decode('utf-8') #Recebe 1024 bites do ponto
+            self.print_(f"{client} disse: {data}")
+
+            if not ip in self.send_to: #Se não tem nada programado em primeiro nível para dizer ao ponto
+                text = self.request(data, ip, port) #Entra na lógica principal do request
+            else: #Se tem algo programado ele diz e apaga aquela mensagem da lista de a dizeres
+                text = self.send_to[ip]
+                del self.send_to[ip]
+
+            if text != None: #Se o texto não for vazio ele manda:
+                con.send(text.encode("utf-8"))
+        else: #Se ele foi bloqueado ele é avisado:
+            con.send("You is blocket!".encode("utf-8"))
+        
+        con.close() #A conexão é fechada
+
+    def request(self, text, ip, port):
+        """
+        Lógica principal.
+        """
+        if text in self.condition: #Se o texto está igual as condições salvas pelo root
+            return self.condition[text]
+
+        elif self.logic != None: #Se existir uma lógica importada ao servidor
+            return self.logic(self, text, ip, port)
+
+    def print_(self, text):
+        """
+        Printa.
+        """
+        if self.print__ == True:
+            print(text)
+
+class Memory:
+    """
+    Memória entre processos.
+    """
+    def __init__(self):
+        self.save = None
+
+def see_txt(name:str, text_old):
+    """
+    Lê o comando manual do root.
+    """
+    with open(name, 'r') as arq:
+        text = arq.read()
+        text = text.split("\n")
+        if text_old.save != text:
+            print(f"Comando mandado <{text}>")
+            text_old.save = text
+            return text
+        else:
+            return None
+
+def comand_line(memory, server):
+    """
+    Lógica da manipulação do servidor pelo root por meio do txt 'comando.txt'.
+    """
+    print(f"""{'-'*50}\nComandos:
+<código> [variável] descrição
+
+
+
+<print> diz se o server deve ou não printar as entradas
+
+<exit> sai do programa
+<ips> vê os ips conectados ou que já conectaram em algum momento
+<to send> mostra a próxima mensagem que vai mandar a cada ip
+
+<block [ip]> bloqueia um ip
+<send_to [ip]> salva a próxima mensagem a mandar para um ip
+<if [algo] [faz algo]> salva uma condicional para mandar para os pontos caso satisfaça a condição
+
+
+
+server.points
+server.equivalent
+server.send_to
+server.condition
+{'-' * 50}\n\n""")
+    
+    text_old = Memory()
+    while True:
+        sleep(0.5)
+        
+        text_ = see_txt("comando.txt", text_old)
+
+        if text_ != None:
+            for text in text_:
+                print(text)
+                #Salvando texto:
+                if text != "":
+                    memory.save = text
+                else:
+                    memory.save = None
+
+                #Mudando definições no server:
+                if text == "print":
+                    print(f"Mudando estado do print do server de {server.print__} para ", end = "")
+                    if server.print__:
+                        server.print__ = False
+                        print("False\n")
+                    else:
+                        server.print__ = True
+                        print("True\n")
+
+                #Informacoes:
+                elif text == "exit":
+                    print("Terminando coneção!\n")
+                    return None            
+
+                elif text == "ips":
+                    for key in server.equivalent["ip"]:
+                        print(f"{key} -> {server.equivalent['ip'][key]}")
+                    print("\n")
+
+                elif text == "to send":
+                    for key in server.send_to:
+                        print(f"{key} -> {server.send_to[key]}")
+                    print("\n")
+
+                #Operações:
+                elif text.find("block") > -1:
+                    try:
+                        new_text = text.split(" ")
+                        if len(new_text[1]) <= 6:
+                            new_text[1] = server.equivalent["int"][int(new_text[1])]
+
+                        server.points[new_text[1]]["block"] = True
+                        print(f"{new_text[1]} foi bloqueado!")
+                    except KeyError:
+                        print(f"'{new_text[1]}' não está registrado.")
+
+                elif text.find("send_to") > -1:
+                    new_text = text.split(" ")
+                    if len(new_text[1]) <= 6:
+                        new_text[1] = server.equivalent["int"][int(new_text[1])]
+
+                    send_text = ""
+                    for i in range(2, len(new_text)):
+                        send_text += new_text[i] + " "
+                        
+                    server.send_to[new_text[1]] = send_text
+                    print(f"Mensagem salva para envio.")
+
+                elif text.find("if") > -1:
+                    new_text = text.split(" ")
+                    new_text[1] = new_text[1].replace("#"," ")
+                        
+                    server.condition[new_text[1]] = new_text[2]
+                    print(f"Resposta definida: {new_text[1]} -> {new_text[2]}\n")
+                
+
+def run_server(server, memory):
+    """
+    Loop principal no thread
+    """
+    while True:
+        server.run_server(memory)
+
+if __name__ == "__main__":
+    memory = Memory()
+    server = Server()
+
+    process_server = Thread(target = run_server, args = [server, memory])
+    process_server.start()
+    
+    comand_line(memory, server) #Está na thread main
+    process_server.join()
+
